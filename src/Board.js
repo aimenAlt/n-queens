ff0b27f86bc9beea9578161686b9df7b1e5eb5cf
Alexei Sorokine <2picae@gmail.com>
2019-01-11 15:50:34 -0800

    Add has row conflicts and any row conflicts

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,11 +79,27 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
+      console.log(this.get());
+      var row = this.get(rowIndex);
+      var found = false;
+      for (var i = 0; i < row.length; i++) {
+        if (row[i] === 1 && found === true) {
+          return true;
+        } else if (row[i] === 1 && found === false) {
+          found = true;
+        }
+      }
       return false; // fixme
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      var currentBoard = this.rows();
+      for (var i = 0; i < currentBoard.length; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
       return false; // fixme
     },
 
